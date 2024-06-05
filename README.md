d6c6e0e07ca41fd187d7deef124c6b98faca43be
Jeon Won <j2onwon@gmail.com>
2024-06-05 23:12:05 +0900

    Add discord/discord.py

--- README.md
+++ README.md
@@ -8,19 +8,30 @@
 일부 코드에서 API 키를 사용할 수 있습니다.
 
 ```python
+FLASK_PORT=8000
+DISCORD_WEBHOOK_URL="https://discord.com/api/hooks/dIsC0RdHo0KuRl
+
 OKX_API_KEY="oKxApIkEy"
 OKX_API_SECRET_KEY="oKxApIsEcReTkEy"
 OKX_API_PASSPHRASE="oKxApIpAsSpHrAsE"
 
-FLASK_PORT=8000
 ```
+
+
+## discord
+
+### discord.py
+
+디스코드 메시지를 보내는 모듈. 환경변수에 디스코드 채팅 채널 웹훅 주소를 등록해야 합니다.
+
+* `send_discord_message()`: 웹훅을 사용하여 디스코드 채팅 채널에 메시지 전송
 
 
 ## okx
 
 ### future.py
 
-OKX 거래소 선물 거래 관련 모듈. OKX 거래소 API 키가 필요하며, 아래 함수들을 사용합니다.
+OKX 거래소 선물 거래 관련 모듈. 환경변수에 OKX 거래소 API 키를 등록해야 합니다.
 
 * `set_leverage()`: 레버리지 설정
 * `create_market_order()`: 시장가 주문 체결
---
+++ discord/discord.py
@@ -0,0 +1,31 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
+
+def send_discord_message(webhook_url, message):
+    """디스코드 웹훅을 사용하여 메시지를 보냅니다.
+
+    Args:
+      - webhook_url: 디스코드 채널 웹훅 URL
+      - message: 보내려는 메시지(마크다운 일부 형식도 가능함)
+
+    Returns: <class 'NoneType'>
+    """
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'content': message,
+    }
+
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 204:
+        print('Message successfully sent to Discord channel.')
+    else:
+        print(f'Failed to send message to Discord channel. Status code: {response.status_code}')
